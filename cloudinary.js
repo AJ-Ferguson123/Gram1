78f7bd1a2f9e6d1abca35046611fd2ff93b53d56
AJ-Ferguson123 <50592799+AJ-Ferguson123@users.noreply.github.com>
2021-11-04 16:15:34 -0600

    cloudinary
    set up api

---
+++ cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary')
+    cloudinary.config({
+        cloud_name: 'dlitujslv',
+        api_key: '827738885521247',
+        api_secret: 'wacBCj-4tfNyMp_HnYDXVmNOLsw'
+    });
+
+    module.exports = {cloudinary}
